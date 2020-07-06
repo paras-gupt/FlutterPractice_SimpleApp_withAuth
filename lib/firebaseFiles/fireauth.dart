74eddd47b90b897e3334f5507a8f429033cdbf84
paras-gupt <59513189+paras-gupt@users.noreply.github.com>
2020-07-06 23:28:11 +0530

    Update fireauth.dart

--- lib/firebaseFiles/fireauth.dart
+++ lib/firebaseFiles/fireauth.dart
@@ -6,12 +6,10 @@ class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // create user obj based on firebase user
   User _userFromFirebaseUser(FirebaseUser user) {
     return user != null ? User(uid: user.uid) : null;
   }
 
-  // auth change user stream
   Stream<User> get user {
     return _auth.onAuthStateChanged
     //.map((FirebaseUser user) => _userFromFirebaseUser(user));
@@ -19,7 +17,6 @@ class AuthService {
   }
 
 
-  // sign in with email and password
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
       AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
@@ -31,12 +28,10 @@ class AuthService {
     }
   }
 
-  // register with email and password
   Future registerWithEmailAndPassword(String name, String phone, String clg, String email, String password) async {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
-      // create a new document for the user with the uid
       await DatabaseService(uid: user.uid).updateUserData(email, name, phone, clg, <String>[] );
       return _userFromFirebaseUser(user);
     } catch (error) {
@@ -45,7 +40,6 @@ class AuthService {
     }
   }
 
-  // sign out
   Future signOut() async {
     try {
       return await _auth.signOut();
